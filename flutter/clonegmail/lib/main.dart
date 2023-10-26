db89ddfc560b592a86a94fb1a958777d0d78fb47
Pushpendra Kumar <pknatic@gmail.com>
2023-10-26 18:27:15 +0530

    update

--- flutter/clonegmail/lib/main.dart
+++ flutter/clonegmail/lib/main.dart
@@ -9,84 +9,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     printinngmail()
     {
-      return SizedBox(
-        height: 90,
-        child: Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .20,
-              child:const CircleAvatar(
-                maxRadius: 25,
-                child: Text("P",style: TextStyle(fontSize: 28),),
+      for(int i =0; i<=10;i++)
+      {
+        return SizedBox(
+          height: 90,
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .20,
+                child:const CircleAvatar(
+                  maxRadius: 25,
+                  child: Text("P",style: TextStyle(fontSize: 28),),
+                ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .65,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("PhonePe",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                  Text("Sent ₹160 to PUSHPENDRA KUMAR",style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),),
-                  Text("PhonePe Oct 22,2023 Paid To PUSHPEND...",style: TextStyle(fontSize: 13),),
-                ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .65,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("PhonePe",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                    Text("Sent ₹160 to PUSHPENDRA KUMAR",style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),),
+                    Text("PhonePe Oct 22,2023 Paid To PUSHPEND...",style: TextStyle(fontSize: 13),),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .15,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("9:15 am",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 13),),
-                  IconButton(onPressed: (){}, icon: const Icon(Icons.star_border)),
-                ],
-              ),
-            )
-          ],
-        ),
-      );
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .15,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("9:15 am",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 13),),
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.star_border)),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      }
     }
-    printinngmailbyvalue(label,sender,subject,mail)
-    {
-      return SizedBox(
-        height: 90,
-        child: Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .20,
-              child:const CircleAvatar(
-                maxRadius: 25,
-                child: Text(label,style: TextStyle(fontSize: 28),),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .65,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("PhonePe",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                  Text("Sent ₹160 to PUSHPENDRA KUMAR",style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),),
-                  Text("PhonePe Oct 22,2023 Paid To PUSHPEND...",style: TextStyle(fontSize: 13),),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .15,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("9:15 am",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 13),),
-                  IconButton(onPressed: (){}, icon: const Icon(Icons.star_border)),
-                ],
-              ),
-            )
-          ],
-        ),
-      );
-    }
+    // printinngmailbyvalue(label,sender,subject,mail)
+    // {
+    //   return SizedBox(
+    //     height: 90,
+    //     child: Row(
+    //       children: [
+    //         SizedBox(
+    //           width: MediaQuery.of(context).size.width * .20,
+    //           child:const CircleAvatar(
+    //             maxRadius: 25,
+    //             child: Text("P",style: TextStyle(fontSize: 28),),
+    //           ),
+    //         ),
+    //         SizedBox(
+    //           width: MediaQuery.of(context).size.width * .65,
+    //           child: const Column(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               Text("PhonePe",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+    //               Text("Sent ₹160 to PUSHPENDRA KUMAR",style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),),
+    //               Text("PhonePe Oct 22,2023 Paid To PUSHPEND...",style: TextStyle(fontSize: 13),),
+    //             ],
+    //           ),
+    //         ),
+    //         SizedBox(
+    //           width: MediaQuery.of(context).size.width * .15,
+    //           child: Column(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               const Text("9:15 am",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 13),),
+    //               IconButton(onPressed: (){}, icon: const Icon(Icons.star_border)),
+    //             ],
+    //           ),
+    //         )
+    //       ],
+    //     ),
+    //   );
+    // }
     return MaterialApp(
       title: "G-mail",
       debugShowCheckedModeBanner: false,
@@ -107,7 +110,6 @@ class MyApp extends StatelessWidget {
         body: ListView(
           children: [
             printinngmail(),
-            printinngmailbyvalue("A", "Amazon", "Order-123515", "your order has been dispatched"),
           ],
         ),
       ),
